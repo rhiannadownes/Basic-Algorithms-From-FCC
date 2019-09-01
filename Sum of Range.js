8c03f8d7be69f947fc297e6acbf73c08891bb91b
Rhianna Downes <rhiannadownes@gmail.com>
2019-09-01 14:33:10 +1000

    Add Else Condition For Decreasing Arrays and Test
    This works for increasing arrays [1, 4] and decreasing arrays [4, 1] and therefore passes FCC test cases. Could still test for same numbers [4, 4] or arrays with negative numbers.

--- Sum of Range.js
+++ Sum of Range.js
@@ -3,10 +3,17 @@ Return the sum of those two numbers plus the sum of all the numbers between them
 The lowest number will not always come first. */
 
 function sumAll(arr) {
-    let integerList = [];
+  let integerList = [];
+  if (arr[0] < arr[1]) {
     for(let i = arr[0]; i <= arr[1]; i++) {
       integerList.push(i);
     }
+  }
+  else if (arr[0] > arr[1]) {
+    for(let i = arr[0]; i >= arr[1]; i--) {
+      integerList.push(i);
+    }
+  }
     return integerList.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   }
   
