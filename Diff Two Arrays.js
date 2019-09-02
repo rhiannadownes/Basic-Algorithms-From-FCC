170150e017c1612a4cbd95e58b2f8a243b49e137
Rhianna Downes <rhiannadownes@gmail.com>
2019-09-02 20:05:32 +1000

    Try If Loop To Identify Duplicates
    Didn't work because even if it identified duplicates (1, 1), it would then fail on the next number in the array (1, 1, 2).

--- Diff Two Arrays.js
+++ Diff Two Arrays.js
@@ -3,14 +3,16 @@
 
 function diffArray(arr1, arr2) {
     let joinedOrderedArray = arr1.concat(arr2).sort();
-    
-    function findUniqueElements(value, index, self) {
-        return self.indexOf(value) === index;
-    } //No, this function just removes duplicates, didn't work
 
-    let newArr = joinedOrderedArray.filter(findUniqueElements);
+    let newArr = [];
+    for (var i = 0; i < joinedOrderedArray.length - 1; i++) {
+      if (joinedOrderedArray[i + 1] !== joinedOrderedArray[i]) {
+        newArr.push(joinedOrderedArray[i]);
+      }
+      //Works for say array[0] but then fails on array[1], not going to work like this
+  }   
     return newArr;
-  }
+}
   
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
